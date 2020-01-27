2872274fda5b4f6c9cfc88f56fc2f6e559a3947e
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-01-26 20:53:30 -0500

    Final commit 1.Added screenshots 2. added product screen

--- ShoppingAppFrontEnd/app/src/main/java/com/example/task3/MainActivity.java
+++ ShoppingAppFrontEnd/app/src/main/java/com/example/task3/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.product_card);
+        setContentView(R.layout.product_screen);
     }
 }
