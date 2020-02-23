6fc366efc80d8abe27c9a90c730384682829b8a9
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-02-23 00:33:06 -0500

    Completed till 3.6

--- TheShoppy/app/src/main/java/com/example/theshoppy/MainActivity.java
+++ TheShoppy/app/src/main/java/com/example/theshoppy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.theshoppy;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -24,25 +27,63 @@ public class MainActivity extends AppCompatActivity {
     Calendar calender;
     Button btnChooseDate;
     AutoCompleteTextView editProvince;
+    RadioGroup radioGrpType;
+    RadioButton radioBtnDesktop;
+    RadioButton radioBtnLaptop;
+    CardView cardDesktops;
+    CardView cardLaptops;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialize();
+        setUp();
     }
-    private void initialize(){
+    private void setUp(){
         //set the app bar to the custom
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         //attach the custom view to the main_activity for the app label
         getSupportActionBar().setCustomView(R.layout.app_bar);
-        //initialize variables
-        editDate = findViewById(R.id.editDate);
-        btnChooseDate = findViewById(R.id.btnChooseDate);
-        editProvince = findViewById(R.id.editProvince);
+        initializeVariables();
         //attach date picker to the button using clickListener
         onClickChooseDate();
         //set the  autoComplete properties int the form
         setupAutoComplete();
+        //set the listener on computer type
+        setUpComputerType();
+
+    }
+
+    private void initializeVariables() {
+        //initialize variables
+        editDate = findViewById(R.id.editDate);
+        btnChooseDate = findViewById(R.id.btnChooseDate);
+        editProvince = findViewById(R.id.editProvince);
+        radioGrpType = findViewById(R.id.radioGrpType);
+        radioBtnDesktop = findViewById(R.id.radioBtnDesktop);
+        radioBtnLaptop = findViewById(R.id.radioBtnLaptop);
+        cardDesktops = findViewById(R.id.cardDesktops);
+        cardLaptops = findViewById(R.id.cardLaptops);
+    }
+
+    private void setUpComputerType() {
+
+        radioGrpType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                //check which radio button is checked
+                if(radioBtnDesktop.isChecked()){
+                    //if desktop is chosen then hide laptop cardview and make desktop visible
+                    cardLaptops.setVisibility(View.GONE);
+                    cardDesktops.setVisibility(View.VISIBLE);
+                }
+                else{
+                    //if laptop is chosen then hide desktop cardview and make laptops visible
+                    cardDesktops.setVisibility(View.GONE);
+                    cardLaptops.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
     }
 
