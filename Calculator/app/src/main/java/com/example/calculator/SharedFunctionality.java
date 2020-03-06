3474c37eec67abe3b2366052339fbe937896ad04
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-03-06 16:58:48 -0500

    Completed with basic functionality for the college project, needs a lot of rework to make it work like a real calculator

--- Calculator/app/src/main/java/com/example/calculator/SharedFunctionality.java
+++ Calculator/app/src/main/java/com/example/calculator/SharedFunctionality.java
@@ -6,15 +6,12 @@ import java.util.Stack;
 
 public final class SharedFunctionality {
     //stacks to handle redo and undo operations
-    private static Stack undoOperators ;
-    private static Stack redoOperators ;
     private static Stack undoResults;
     private static Stack redoResults;
    //variables declaration
     private static StringBuilder resultString;
     private static StringBuilder inputNumberString;
     private static char operator ;
-    private static double number;
     private static boolean check;
     private static double firstNumber;
     /*****setters and getters*****/
@@ -37,20 +34,6 @@ public final class SharedFunctionality {
         }
         inputNumberString.append(value);
     }
-
-    public static void pushUndoOperators(String value){
-        if(undoOperators == null){
-            undoOperators = new Stack();
-        }
-        undoOperators.push(value);
-    }
-
-    public static void pushRedoOperators(String value){
-        if(redoOperators == null){
-            redoOperators = new Stack();
-        }
-        redoOperators.push(value);
-    }
     public static void pushUndoResults(String value){
         if(undoResults == null){
             undoResults = new Stack();
@@ -69,9 +52,6 @@ public final class SharedFunctionality {
         operator = value;
     }
 
-    public static void setNumber(double value){
-        number= value;
-    }
     public static void setComputedCheck(boolean value){
         check = value;
     }
@@ -96,19 +76,6 @@ public final class SharedFunctionality {
         return operator;
     }
 
-    public static String popUndoOperators(){
-        if(undoOperators == null || undoOperators.empty())
-            return "0";
-        else
-            return String.valueOf(undoOperators.pop());
-    }
-    public static String popRedoOperators(){
-        if(redoOperators == null || redoOperators.empty())
-            return "0";
-        else
-            return String.valueOf(redoOperators.pop());
-    }
-
     public static String popUndoResults(){
         if(undoResults == null || undoResults.empty())
             return "0";
@@ -125,30 +92,12 @@ public final class SharedFunctionality {
         return check;
     }
     //stack peekers
-    public static String peekUndoOperators(){
-        if(undoOperators == null || undoOperators.empty())
-            return "0";
-        else
-            return String.valueOf(undoOperators.peek());
-    }
-    public static String peekRedoOperators(){
-        if(redoOperators == null || redoOperators.empty())
-            return "0";
-        else
-            return String.valueOf(redoOperators.peek());
-    }
 
     public static String peekUndoResults(){
         if(undoResults == null || undoResults.empty())
             return "0";
         else
             return String.valueOf(undoResults.peek());
-    }
-    public static String peekRedoResults(){
-        if(redoResults == null || redoResults.empty())
-            return "0";
-        else
-            return String.valueOf(redoResults.peek());
     }
     public static int lengthUndoResults(){
         if(undoResults != null)
@@ -160,29 +109,12 @@ public final class SharedFunctionality {
             return redoResults.size();
         return 0;
     }
-    public static int lengthUndoOperators(){
-        if(undoOperators != null)
-            return undoOperators.size();
-        return 0;
-    }
-    public static int lengthRedoOperators(){
-        if(redoOperators != null)
-            return redoOperators.size();
-        return 0;
-    }
 
-    public static double getNumber(){
-        return number;
-    }
 
     //clear variables
     public static void clear(){
         if( undoResults != null)
             undoResults.clear();
-        if( undoOperators != null)
-            undoOperators.clear();
-        if( redoOperators != null )
-            redoOperators.clear();
         if( redoResults != null)
             redoResults.clear();
         inputNumberString = new StringBuilder();
@@ -193,8 +125,5 @@ public final class SharedFunctionality {
 
     public static void clearInputString(){
         inputNumberString = new StringBuilder();
-    }
-    public static void clearResultString(){
-        resultString = new StringBuilder();
     }
 }
