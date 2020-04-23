6b3eaafade77fdeb4aa5d5c0e492a16bd6df34b7
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-04-23 08:16:35 -0400

    contact selection process finishe

---
+++ SOS/app/src/main/java/com/example/sos/Contact.java
@@ -0,0 +1,20 @@
+package com.example.sos;
+
+public class Contact {
+    String name;
+    String number;
+    //setter and getters
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getNumber(){
+        return number;
+    }
+    public void setNumber (String number){
+        this.number = number;
+    }
+}
--- SOS/app/src/main/java/com/example/sos/MainScreen.java
+++ SOS/app/src/main/java/com/example/sos/MainScreen.java
@@ -1,16 +1,25 @@
 package com.example.sos;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
     ImageButton btnFirstAdd;
@@ -30,6 +39,11 @@ public class MainScreen extends AppCompatActivity {
     LinearLayout contact3;
     int check = 1;
     String contactData[];
+    List<Contact> contacts;
+    SharedPreferences.Editor editor;
+    Gson gson;
+
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +70,54 @@ public class MainScreen extends AppCompatActivity {
         contact1 = findViewById(R.id.firstContact);
         contact2 = findViewById(R.id.secondContact);
         contact3 = findViewById(R.id.thirdContact);
+        sharedPreferences = getSharedPreferences(getString(R.string.app_pref),MODE_PRIVATE);
+        contacts = new ArrayList<Contact>();
+        editor = sharedPreferences.edit();
+        gson = new Gson();
+        setContactList();
+    }
+
+    private void setContactList() {
+        //get already added contacts from the share pref
+        String jsonData = sharedPreferences.getString(getString(R.string.contacts),"");
+        //check if there are any contacts
+        if(!jsonData.isEmpty())
+        {
+            Type type = new TypeToken<List<Contact>>(){}.getType();
+            contacts = gson.fromJson(jsonData,type);
+            Log.i("Contacts",String.valueOf(contacts.size()));
+            contactData = new String[2];
+            //set the data to the
+            if(contacts.size() == 1){
+                contactData[0] = contacts.get(0).getName();
+                contactData[1] = contacts.get(0).getNumber();
+                changeAttributesOnAdd(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
+            }
+            else if(contacts.size() == 2){
+                contactData[0] = contacts.get(0).getName();
+                contactData[1] = contacts.get(0).getNumber();
+                changeAttributesOnAdd(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
+
+
+                contactData[0] = contacts.get(1).getName();
+                contactData[1] = contacts.get(1).getNumber();
+                changeAttributesOnAdd(txtSecondName,txtSecondNumber,contact2,btnSecondAdd,btnSecondRemove);
+            }
+            else if(contacts.size() == 3){
+                contactData[0] = contacts.get(0).getName();
+                contactData[1] = contacts.get(0).getNumber();
+                changeAttributesOnAdd(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
+
+
+                contactData[0] = contacts.get(1).getName();
+                contactData[1] = contacts.get(1).getNumber();
+                changeAttributesOnAdd(txtSecondName,txtSecondNumber,contact2,btnSecondAdd,btnSecondRemove);
+
+                contactData[0] = contacts.get(2).getName();
+                contactData[1] = contacts.get(2).getNumber();
+                changeAttributesOnAdd(txtThirdName,txtThirdNumber,contact3,btnThirdAdd,btnThirdRemove);
+            }
+        }
     }
 
     private void setupSelectContact() {
@@ -70,13 +132,15 @@ public class MainScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pickContact();
-                //set the first contact add button was clicked
+                //set, the first contact add button was clicked
                 check = 1;
             }
         });
         btnFirstRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //remove from shared preferences
+                removeFromSharedPreferences(txtFirstNumber);
                 //change attributes on removing a contact
                 changeAttributesOnRemove(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
             }
@@ -95,6 +159,8 @@ public class MainScreen extends AppCompatActivity {
         btnSecondRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //remove from shared preferences
+                removeFromSharedPreferences(txtSecondNumber);
                 //change attributes on removing a contact
                 changeAttributesOnRemove(txtSecondName,txtSecondNumber,contact2,btnSecondAdd,btnSecondRemove);
             }
@@ -113,6 +179,8 @@ public class MainScreen extends AppCompatActivity {
         btnThirdRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //remove from shared preferences
+                removeFromSharedPreferences(txtThirdNumber);
                 //change attributes on removing a contact
                 changeAttributesOnRemove(txtThirdName,txtThirdNumber,contact3,btnThirdAdd,btnThirdRemove);
             }
@@ -127,19 +195,44 @@ public class MainScreen extends AppCompatActivity {
        if(requestCode == 1 && resultCode == RESULT_OK){
            ContactsHandler handler = new ContactsHandler(this);
            contactData = handler.getContactDetails(data.getData());
+           //check if  contact was already added to the list
+           for(int i=0;i<contacts.size();i++){
+               if(contacts.get(i).getNumber().equals( contactData[1])){
+                   Log.i("Contacts","There");
+                   Toast.makeText(this,"Contact already added!",Toast.LENGTH_LONG).show();
+                   return;
+               }
+           }
            if(check == 1){
                //change attributes of UI elements
-               changeAtributesOnAdd(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
+               changeAttributesOnAdd(txtFirstName,txtFirstNumber,contact1,btnFirstAdd,btnFirstRemove);
+              //store in shared preference
+               storeSharedPreferences();
            }
            else if(check == 2){
                //change attributes of UI elements
-               changeAtributesOnAdd(txtSecondName,txtSecondNumber,contact2,btnSecondAdd,btnSecondRemove);
+               changeAttributesOnAdd(txtSecondName,txtSecondNumber,contact2,btnSecondAdd,btnSecondRemove);
+               //store in shared preference
+               storeSharedPreferences();
            }
            else if(check == 3){
                //change attributes of UI elements
-               changeAtributesOnAdd(txtThirdName,txtThirdNumber,contact3,btnThirdAdd,btnThirdRemove);
+               changeAttributesOnAdd(txtThirdName,txtThirdNumber,contact3,btnThirdAdd,btnThirdRemove);
+               //store in shared preference
+               storeSharedPreferences();
            }
        }
+   }
+
+   public void storeSharedPreferences(){
+       Contact contact =  new Contact();
+       contact.setName(contactData[0]);
+       contact.setNumber(contactData[1]);
+       //add it to the list
+       contacts.add(contact);
+       String jsonData = gson.toJson(contacts);
+       editor.putString(getString(R.string.contacts),jsonData);
+       editor.commit();
    }
 
    public void pickContact(){
@@ -147,7 +240,7 @@ public class MainScreen extends AppCompatActivity {
        startActivityForResult(contactPicker,1);
    }
 
-   public void changeAtributesOnAdd(TextView name,TextView number, LinearLayout contact, ImageButton add, ImageButton remove){
+   public void changeAttributesOnAdd(TextView name, TextView number, LinearLayout contact, ImageButton add, ImageButton remove){
         //set data
        name.setText(contactData[0]);
        number.setText(contactData[1]);
@@ -165,4 +258,19 @@ public class MainScreen extends AppCompatActivity {
        add.setVisibility(View.VISIBLE);
        remove.setVisibility(View.GONE);
    }
+    private void removeFromSharedPreferences(TextView txtNumber) {
+
+        for(int i= 0;i<contacts.size();i++){
+            if(txtNumber.getText().toString().equals(contacts.get(i).getNumber())){
+                contacts.remove(i);
+            }
+        }
+
+        //update shared pref
+        String jsonData = gson.toJson(contacts);
+        editor.putString(getString(R.string.contacts),jsonData);
+        editor.commit();
+    }
+
+
 }
